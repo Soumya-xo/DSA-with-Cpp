03c163dcef0f7956a19f7dcf3cbea6e425c99d3e
Soumya Nayak <soumyanayakk08@gmail.com>
2026-06-11 14:29:22 +0530

    Update DSA codes

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -32,3 +32,31 @@ int main() {
     printArray(arr,n);
     return 0;
 }
+
+
+
+
+
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int mid = 0;
+        int high = nums.size()-1;
+        int low = 0;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                mid++;
+                low++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+    }
+};
